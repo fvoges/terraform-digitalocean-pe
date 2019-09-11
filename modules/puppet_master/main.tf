c217b4c1c37961c2cc6c2763e833a518229ba1f3
Federico Voges <federico@hashicorp.com>
2019-09-11 14:26:15 +0100

    update to new syntax

--- modules/puppet_master/main.tf
+++ modules/puppet_master/main.tf
@@ -11,18 +11,17 @@ resource "digitalocean_tag" "agent" {
 }
 
 resource "digitalocean_droplet" "master" {
-  image              = "${var.do_image}"
+  image              = var.do_image
   name               = "master"
-  region             = "${var.do_region}"
-  size               = "${var.do_master_size}"
+  region             = var.do_region
+  size               = var.do_master_size
   private_networking = true
   monitoring         = true
   resize_disk        = true
   ipv6               = true
   ssh_keys           = var.do_ssh_fingerprints
-  ]
-  tags      = ["all", "puppet-master", "ssh4all"]
-  user_data = "${data.template_file.master_userdata.rendered}"
+  tags               = [ "all", "puppet-master", "ssh4all" ]
+  user_data          = data.template_file.master_userdata.rendered
 
   depends_on = [
     digitalocean_tag.master,
@@ -50,12 +49,12 @@ resource "digitalocean_droplet" "master" {
   }
 
   provisioner "file" {
-    content     = "${data.template_file.custom_pe_conf.rendered}"
+    content     = data.template_file.custom_pe_conf.rendered
     destination = "/opt/terraform/data/custom-pe.conf"
   }
 
   provisioner "file" {
-    source      = "${path.module}/files/data/master_csr_attributes.yaml"
+    content     = data.template_file.master_csr_attributes.rendered
     destination = "/opt/terraform/data/csr_attributes.yaml"
   }
 
@@ -65,22 +64,22 @@ resource "digitalocean_droplet" "master" {
   }
 
   provisioner "file" {
-    content     = "${data.template_file.provision_master_script.rendered}"
+    content     = data.template_file.provision_master_script.rendered
     destination = "/opt/terraform/scripts/provision_master.sh"
   }
 
   provisioner "file" {
-    content     = "${data.template_file.master_classification.rendered}"
+    content     = data.template_file.master_classification.rendered
     destination = "/opt/terraform/manifests/classification.pp"
   }
 
   provisioner "file" {
-    content     = "${data.template_file.master_hiera_data.rendered}"
+    content     = data.template_file.master_hiera_data.rendered
     destination = "/opt/terraform/data/common.yaml"
   }
 
   provisioner "file" {
-    content     = "${data.template_file.master_hiera_data.rendered}"
+    content     = data.template_file.master_hiera_data.rendered
     destination = "/etc/puppetlabs/code/environments/production/data/common.yaml"
   }
 
@@ -106,63 +105,63 @@ resource "digitalocean_droplet" "master" {
 }
 
 resource "digitalocean_record" "master" {
-  domain = "${var.do_domain}"
+  domain = var.do_domain
   type   = "A"
-  name   = "${digitalocean_droplet.master.name}"
-  value  = "${digitalocean_droplet.master.ipv4_address}"
+  name   = digitalocean_droplet.master.name
+  value  = digitalocean_droplet.master.ipv4_address
   ttl    = 60
 }
 
 resource "digitalocean_firewall" "master" {
   name = "puppet-master"
-  tags = ["puppet-master"]
+  tags = [ "puppet-master" ]
 
 
   inbound_rule {
       protocol         = "tcp"
       port_range       = "443"
-      source_addresses = ["0.0.0.0/0", "::/0"]
+      source_addresses = [ "0.0.0.0/0", "::/0" ]
   }
 
   inbound_rule {
       protocol         = "tcp"
       port_range       = "8170"
-      source_addresses = ["0.0.0.0/0", "::/0"]
+      source_addresses = [ "0.0.0.0/0", "::/0" ]
   }
 
   inbound_rule {
       protocol    = "tcp"
       port_range  = "8140"
-      source_tags = ["puppet-compiler", "puppet-agent"]
+      source_tags = [ "puppet-compiler", "puppet-agent" ]
   }
 
   inbound_rule {
       protocol    = "tcp"
       port_range  = "8142"
-      source_tags = ["puppet-compiler", "puppet-agent"]
+      source_tags = [ "puppet-compiler", "puppet-agent" ]
   }
 
   inbound_rule {
       protocol    = "tcp"
       port_range  = "4433"
-      source_tags = ["puppet-compiler"]
+      source_tags = [ "puppet-compiler" ]
   }
 
   inbound_rule {
       protocol    = "tcp"
       port_range  = "5432"
-      source_tags = ["puppet-compiler"]
+      source_tags = [ "puppet-compiler" ]
   }
 
   inbound_rule {
       protocol    = "tcp"
       port_range  = "8081"
-      source_tags = ["puppet-compiler"]
+      source_tags = [ "puppet-compiler" ]
   }
 
   inbound_rule {
       protocol    = "tcp"
       port_range  = "8143"
-      source_tags = ["puppet-compiler"]
+      source_tags = [ "puppet-compiler" ]
   }
 }
