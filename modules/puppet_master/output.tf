69f15cc2bfac2d47c3bc967f581d1769335b793f
Federico Voges <federico@hashicorp.com>
2019-09-11 14:27:17 +0100

    cleanup

--- modules/puppet_master/output.tf
+++ modules/puppet_master/output.tf
@@ -1,6 +1,7 @@
 output "ipv4_address" {
-  value = "${digitalocean_droplet.master.ipv4_address}"
+  value = digitalocean_droplet.master.ipv4_address
 }
+
 output "name" {
-  value = "${digitalocean_droplet.master.name}"
+  value = "${digitalocean_droplet.master.name}.${var.do_domain}"
 }
