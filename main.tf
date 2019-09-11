9d38726dcd522aaa4e3109215671ebba2475238a
Federico Voges <federico@hashicorp.com>
2019-09-11 14:34:31 +0100

    add puppet compiler module

--- main.tf
+++ main.tf
@@ -19,6 +19,16 @@ module "puppet_master" {
   do_domain           = var.do_domain
   do_ssh_fingerprints = var.do_ssh_fingerprints
 }
+
+module "puppet_compiler" {
+  source = "./modules/puppet_compiler"
+
+  compiler_count      = "1"
+  master              = module.puppet_master.name
+  master_ipv4         = module.puppet_master.ipv4_address
+  do_region           = var.do_region
+  do_domain           = var.do_domain
+  do_ssh_fingerprints = var.do_ssh_fingerprints
 }
 
 output "puppet_master_name" {
