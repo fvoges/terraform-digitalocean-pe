aae36e59bd0b041129f842aa61fff91997139113
Federico Voges <federico@hashicorp.com>
2019-09-11 14:25:04 +0100

    update inputs and outputs to match module

--- main.tf
+++ main.tf
@@ -15,10 +15,15 @@ terraform {
 module "puppet_master" {
   source = "./modules/puppet_master"
 
+  do_region           = var.do_region
+  do_domain           = var.do_domain
   do_ssh_fingerprints = var.do_ssh_fingerprints
 }
 }
 
 output "puppet_master_name" {
-  value = "puppet_master.name"
+  value = module.puppet_master.name
+}
+output "puppet_master_ipv4_address" {
+  value = module.puppet_master.ipv4_address
 }
